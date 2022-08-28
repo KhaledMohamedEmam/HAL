3e954d11ba30ac81be20cf6154243b73bc482092
KhaledMohamedEmam <112343705+KhaledMohamedEmam@users.noreply.github.com>
2022-08-28 22:38:05 +0200

    Update lcd_int.h

--- lcd_int.h
+++ lcd_int.h
@@ -4,7 +4,6 @@ void HLCD_SendCommand(u8 A_u8_command);
 void HLCDSendData(u8 A_u8_data);
 void HLCD_SendString(u8 *ptr_string);
 void Lcd_SendInt(u16 value);
-void timer0_FPWMInit();
 void HLCD_vGoToRowCol(u8 A_u8Row, u8 A_u8Col);
 
 
